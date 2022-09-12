f8de1218a5a950d049d7f444a7bacac4b3c2047c
azu <mmkazumi@gmail.com>
2022-09-12 14:00:44 +0900

    管理除外

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	tabWidth: 4, //インデントのスペース数
-	singleQuote: true, //シングルクォートに統一
-	trailingComma: 'es5', //末尾のカンマをどうするか: es5に準拠させる。
-	useTabs: true, // インデントにタブを使用する
-	bracketSpacing: true, // カッコ内のスペース
-};
