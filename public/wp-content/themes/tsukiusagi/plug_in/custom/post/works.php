bc1dd1a7cb164826f737f868ba58bbcfa263a686
azu <mmkazumi@gmail.com>
2022-09-13 20:08:00 +0900

    update functions

--- public/wp-content/themes/tsukiusagi/plug_in/custom/post/works.php
+++ public/wp-content/themes/tsukiusagi/plug_in/custom/post/works.php
@@ -1,16 +1,17 @@
 <?php
+
 /**
-* Plugin Name: custom_post
-* Plugin URI: 
-* Description: カスタムタクソノミー追加
-* Version: 1.0
-* Author: tsukiusagi.biz
-* Author URI: https://tsukiusagi.biz
-* License: 
-* 
-* @param array $array：スラッグ、名前
-* 
-*/
+ * Plugin Name: custom_post
+ * Plugin URI:
+ * Description: カスタムタクソノミー追加
+ * Version: 1.0
+ * Author: tsukiusagi.biz
+ * Author URI: https://tsukiusagi.biz
+ * License:
+ *
+ * @param array $array：スラッグ、名前
+ *
+ */
 //--------------------------------------------
 // カスタム投稿・タクソノミー
 //--------------------------------------------
@@ -19,7 +20,8 @@ $post_works_array = array(
     'name'      => 'works',
     'icon'      => 'dashicons-editor-paste-word',
 );
-function create_post_works($array){
+
+function create_post_works($array) {
     $post_args = usa_return_custom_post_args($array);
     $category_args = usa_return_custom_cat_args($array);
     $tag_args = usa_return_custom_tag_args($array);
@@ -32,17 +34,17 @@ function create_post_works($array){
 
     // カスタムタクソノミー（カテゴリー）の登録
     register_taxonomy(
-        $array['post_type'].'-category',  // 1.スラッグ：$term->slug
+        $array['post_type'] . '-category',  // 1.スラッグ：$term->slug
         $array['post_type'],              // 2.利用する投稿タイプ
         $category_args                    // 3.オプション
     );
 
     // カスタムタクソノミー（タグ）の設定
     register_taxonomy(
-        $array['post_type'].'-tag',
-        $array['post_type'] ,
+        $array['post_type'] . '-tag',
+        $array['post_type'],
         $tag_args
     );
 }
-add_action('hook_create_post_works', 'create_post_works',10,1);
-do_action('hook_create_post_works',$post_works_array);
+add_action('hook_create_post_works', 'create_post_works', 10, 1);
+do_action('hook_create_post_works', $post_works_array);
