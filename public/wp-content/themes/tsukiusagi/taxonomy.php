0b72dd9a7645b8064f36acce44ef18b4a5946a99
azu <mmkazumi@gmail.com>
2022-09-13 15:01:03 +0900

    update taxonomy.php

--- public/wp-content/themes/tsukiusagi/taxonomy.php
+++ public/wp-content/themes/tsukiusagi/taxonomy.php
@@ -1,6 +1,6 @@
-<?php get_header(); ?>
-
 <?php
+get_header();
+
 $term = wp_get_object_terms($post->ID, 'カスタムタクソノミー名');
 $args = array(
   'post_type' => 'works',
@@ -9,18 +9,15 @@ $args = array(
   'posts_per_page' => 10,
 );
 $custom_query = new WP_Query($args);
-if ($custom_query->have_posts()) :
-  while ($custom_query->have_posts()) :
+
+if ($custom_query->have_posts()) {
+  while ($custom_query->have_posts()) {
     $custom_query->the_post();
-?>
 
-<?php if (get_post_type() === 'works') : ?>
-  <?php get_template_part('components/template/loop/sub'); ?>
-<?php endif; ?>
-
-<?php endwhile;
-endif;
+    if (get_post_type() === 'works') {
+      get_template_part('components/template/loop/sub');
+    }
+  }
+}
 wp_reset_postdata();
-?>
-
-<?php get_footer(); ?>
+get_footer();
