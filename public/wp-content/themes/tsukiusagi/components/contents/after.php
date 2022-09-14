538b0c7fd76256d400dfd51dc499f5dd99c6c4f1
azu <mmkazumi@gmail.com>
2022-09-14 16:33:43 +0900

    220914 update ball-box

--- public/wp-content/themes/tsukiusagi/components/contents/after.php
+++ public/wp-content/themes/tsukiusagi/components/contents/after.php
@@ -5,33 +5,24 @@ $main_inner_class = $main_class . '__inner';
 $ex_inner_class = $ex_class . '__inner';
 // $bg_class = 'c-bg--gradient-transparent';
 $bg_class = 'c-bg--before-footer';
+?>
 
-// *section_start*
-if (is_single()) {
-  echo '<section class="' . $main_class . ' ' . $bg_class . '">';
-} else {
-  echo '<section class="' . $ex_class .  ' ' . $bg_class . '">';
-};
+<?php if (is_single()) : ?>
+  <section class="<?php echo $main_class; ?> <?php echo $bg_class; ?>">
+  <?php else : ?>
+    <section class="<?php echo $ex_class; ?> <?php echo $bg_class; ?>">
+    <?php endif; ?>
 
+    <?php if (is_single()) : ?>
+      <article class="<?php echo $main_inner_class; ?>">
+        <?php get_template_part('components/parts/ball'); ?>
+        <?php get_template_part('components/text/list-ttl'); ?>
+        <?php get_template_part('components/template/loop/sub'); ?>
 
-// *article_start*
-// var_single
-if (is_single()) {
-  echo '<article class="' . $main_inner_class . '">';
-  get_template_part('components/bg/cosmos/crystal');
+      <?php elseif (is_post_type_archive('works')) : ?>
+        <article class="<?php echo $ex_inner_class; ?>">
+        <?php endif; ?>
 
-  // if (get_post_type() === 'works') {
-  //   get_template_part('components/parts/ball');
-  // }
-  get_template_part('components/parts/ball');
-  get_template_part('components/text/list-ttl');
-  get_template_part('components/template/loop/sub');
-
-  // var_archive
-} elseif (is_post_type_archive('works')) {
-  echo '<article class="' . $ex_inner_class . '">';
-};
-
-// *article & section_end*
-get_template_part('components/text/offset-title');
-echo '</article></section>';
+        <?php get_template_part('components/text/offset-title'); ?>
+        </article>
+    </section>
