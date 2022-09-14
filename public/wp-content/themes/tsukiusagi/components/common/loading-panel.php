bc4b8a776fff8084d0b978e39a7591539bd4410c
azu <mmkazumi@gmail.com>
2022-09-14 11:12:07 +0900

    220914 update loading-panel

--- public/wp-content/themes/tsukiusagi/components/common/loading-panel.php
+++ public/wp-content/themes/tsukiusagi/components/common/loading-panel.php
@@ -1,10 +1,10 @@
 <div class="p-loading-panel">
   <div class="p-loading-panel__clouds">
-    <div class="c-loading-clouds"></div>
+    <div class="p-loading-clouds"></div>
   </div>
 
   <div class="p-loading-panel__curtain">
-    <div class="c-loading-curtain--left"></div>
-    <div class="c-loading-curtain--right"></div>
+    <div class="p-loading-curtain--left"></div>
+    <div class="p-loading-curtain--right"></div>
   </div>
 </div>
