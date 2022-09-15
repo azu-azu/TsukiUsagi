f5f240b9f6916b5f97c722da16d36a0641936693
azu <mmkazumi@gmail.com>
2022-09-15 13:06:43 +0900

    220915 update sea-animation

--- public/wp-content/themes/tsukiusagi/components/parts/first-view.php
+++ public/wp-content/themes/tsukiusagi/components/parts/first-view.php
@@ -1,4 +1,4 @@
-<article class="p-first-view c-anim-black-hole">
+<article class="p-first-view">
   <article class="p-first-view__inner">
     <?php get_template_part('components/bg/cosmos/stars'); ?>
     <?php get_template_part('components/common/train'); ?>
