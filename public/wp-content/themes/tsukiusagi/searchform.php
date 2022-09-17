bcef7d8fa2af433d48a14a88cddb8d2c22cc6c78
azu <mmkazumi@gmail.com>
2022-09-17 20:58:24 +0900

    220917 update search

--- public/wp-content/themes/tsukiusagi/searchform.php
+++ public/wp-content/themes/tsukiusagi/searchform.php
@@ -3,6 +3,7 @@
         <label class="screen-reader-text" for="s">検索:</label>
         <input type="text" placeholder="" id="s" name="s" size="20" placeholder="
         <?php if (!is_search()) echo 'SEARCH'; ?>" value="<?php if (is_search()) echo get_search_query(); ?>" /><!-- 検索後も文字を残す -->
+        <label class="place-holder" for="s">記事内検索</label>
         <button type="submit">
             <i class="c-searchform__icon fas fa-search"></i>
         </button>
