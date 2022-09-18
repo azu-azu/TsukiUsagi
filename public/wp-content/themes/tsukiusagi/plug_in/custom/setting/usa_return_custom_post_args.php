9156224bc61b142510c8d19e0314dfa359c9ce1f
azu <mmkazumi@gmail.com>
2022-09-18 10:42:50 +0900

    220918 update custom-post

--- public/wp-content/themes/tsukiusagi/plug_in/custom/setting/usa_return_custom_post_args.php
+++ public/wp-content/themes/tsukiusagi/plug_in/custom/setting/usa_return_custom_post_args.php
@@ -1,18 +1,19 @@
 <?php
-/**
-* Plugin Name: custom_post
-* Plugin URI: 
-* Description: カスタム投稿作成
-* Version: 1.0
-* Author: tsukiusagi.biz
-* Author URI: https://tsukiusagi.biz
-* License: 
-* 
-* @param array $array：スラッグ、名前、アイコン
-* 
-*/
 
-function usa_return_custom_post_args($array){
+/**
+ * Plugin Name: custom_post
+ * Plugin URI:
+ * Description: カスタム投稿作成
+ * Version: 1.0
+ * Author: tsukiusagi.biz
+ * Author URI: https://tsukiusagi.biz
+ * License:
+ *
+ * @param array $array：スラッグ、名前、アイコン
+ *
+ */
+
+function usa_return_custom_post_args($array) {
     $post_type = $array['post_type'];
     $name = $array['name'];
     $icon = $array['icon'];
@@ -25,16 +26,18 @@ function usa_return_custom_post_args($array){
     );
     $args = array(
         'labels' => [
-            'name'          => $name,
+            'name'          => $name . 'の一覧',
             'singular_name' => $post_type,
+            'add_new'       => $name . 'の新規追加', // 日本語表記にする
+            'edit_item'     => '投稿を編集',
         ],
-        'public'        => true,
-        'has_archive'   => true,
-        'menu_position' => 5,
-        'rewrite'       => array('slug' =>$post_type, 'with_front' => false),
+        'public'        => true, // 管理画面に追加
+        'has_archive'   => true, // 投稿した記事の一覧ページを作成する
+        'menu_position' => 5, // 管理画面メニューの表示位置（投稿の下に追加）
+        'rewrite'       => array('slug' => $post_type, 'with_front' => false),
         'show_in_rest'  => true,  // true:「Gutenberg」/ false:「ClassicEditor」
-        'menu_icon' => $icon,
-        'supports'      => $supports
+        'menu_icon'     => $icon,
+        'supports'      => $supports,
     );
     return $args;
 }
--- public/wp-content/themes/tsukiusagi/plug_in/custom/setting/usa_return_custom_tag_args.php
+++ public/wp-content/themes/tsukiusagi/plug_in/custom/setting/usa_return_custom_tag_args.php
@@ -1,21 +1,22 @@
 <?php
+
 /**
-* Plugin Name: custom_post
-* Plugin URI: 
-* Description: カスタムタクソノミー追加
-* Version: 1.0
-* Author: tsukiusagi.biz
-* Author URI: https://tsukiusagi.biz
-* License: 
-* 
-* @param array $array：スラッグ、名前
-* 
-*/
-function usa_return_custom_tag_args($array){
+ * Plugin Name: custom_post
+ * Plugin URI:
+ * Description: カスタムタクソノミー追加
+ * Version: 1.0
+ * Author: tsukiusagi.biz
+ * Author URI: https://tsukiusagi.biz
+ * License:
+ *
+ * @param array $array：スラッグ、名前
+ *
+ */
+function usa_return_custom_tag_args($array) {
     $name = $array['name'];
     $args = array(
         'hierarchical' => true,
-        'label' => '分類', 
+        'label' => $name . 'のタグ',
         'public' => true,
         'show_in_rest' => true //管理画面に追加
     );
