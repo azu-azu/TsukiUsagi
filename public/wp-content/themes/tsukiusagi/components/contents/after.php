6eb4493f2790f08fb4b0732bc0fac0496f41c133
azu <mmkazumi@gmail.com>
2022-09-20 13:54:00 +0900

    220920 update contents-area

--- public/wp-content/themes/tsukiusagi/components/contents/after.php
+++ public/wp-content/themes/tsukiusagi/components/contents/after.php
@@ -1,4 +1,3 @@
 <section class="p-after-contents c-bg--before-footer">
   <?php if (get_post_type() !== 'works') get_sidebar(); ?>
-  <?php get_template_part('components/text/offset-title'); ?>
 </section>
--- public/wp-content/themes/tsukiusagi/components/contents/all.php
+++ public/wp-content/themes/tsukiusagi/components/contents/all.php
@@ -3,18 +3,6 @@ get_header();
 get_template_part('components/common/to-top');
 get_template_part('components/parts/first-view');
 get_template_part('components/header/lower');
-?>
 
-<?php if (is_category() || is_tag() || is_tax()) : ?>
-  <?php get_template_part('components/contents/main'); ?>
-
-<?php else : ?>
-  <?php if (get_post_type() === 'post' && !is_archive() && !is_search()) : ?>
-    <!-- <div class="p-flowing-clouds sp-none"></div> -->
-  <?php endif; ?>
-
-  <?php get_template_part('components/contents/main'); ?>
-<?php endif; ?>
-
-<?php
+get_template_part('components/contents/main');
 get_footer();
