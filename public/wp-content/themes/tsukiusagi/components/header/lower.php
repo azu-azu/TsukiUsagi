c31ec57cbefa7c96bbc6300f432f8cc2d3b94822
azu <mmkazumi@gmail.com>
2022-09-22 13:59:14 +0900

    220922 update header-lower

--- public/wp-content/themes/tsukiusagi/components/header/lower.php
+++ public/wp-content/themes/tsukiusagi/components/header/lower.php
@@ -4,10 +4,9 @@
   <article class="p-header p-header--back">
     <nav class="p-gmenu">
       <ul class="c-nav">
-        <li class="js-ham-close" style="--i:0;"><a class="c-bubbly-button--white" href="<?php echo get_post_type_archive_link('works'); ?>">Works</a></li>
+        <li class="js-ham-close" style="--i:3;"><a class="c-bubbly-button--yellow" href="<?php echo esc_url(home_url('/')); ?>/blog">Blog</a></li>
         <li class="js-ham-close" style="--i:1;"><a class="c-bubbly-button--white" href="<?php echo esc_url(home_url('/')); ?>#profile">Profile</a></li>
         <li class="js-ham-close" style="--i:2;"><a class="c-bubbly-button--yellow" href="<?php echo esc_url(home_url('/')); ?>#contact">Contact</a></li>
-        <li class="js-ham-close" style="--i:3;"><a class="c-bubbly-button--yellow" href="<?php echo esc_url(home_url('/')); ?>/blog">Blog</a></li>
         <div class="c-marker"></div>
       </ul>
     </nav>
