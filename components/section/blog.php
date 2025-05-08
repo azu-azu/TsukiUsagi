df1e061628e40bb1b76ed8825d83d377cfa5e9c7
azu <mmkazumi@gmail.com>
2025-05-08 19:23:38 +0900

    update top 20250508

--- components/section/blog.php
+++ components/section/blog.php
@@ -5,23 +5,36 @@ $bg_class = 'p-layer--f1 c-bg--gradient-f1';
 
 $title = 'Blog';
 
-$args = array(
-  'post_status' => 'publish',
-  "post_type" => "post",
-  "category_name" => "learn", // カテゴリをスラッグで指定する場合
-  "posts_per_page" => 4,
-  // "orderby" => "rand", // ランダムの場合
-  "orderby" => "date", // 新着順
-  'order' => 'DESC', // 念のため
-  "post__not_in" => array(), //現在のページを除外する場合：しない場合は空にする
+// 共通のループ条件
+$base_args = array(
+  'post_status'    => 'publish',
+  'post_type'      => 'post',
+  'orderby'        => 'date',
+  'order'          => 'DESC',
+  'post__not_in'   => array(), // 除外なし
 );
+
+// sqlカテゴリ（2件）
+$args_sql = array_merge($base_args, array(
+  'category_name'  => 'sql',
+  'posts_per_page' => 2,
+));
+
+// aiカテゴリ（2件）
+$args_ai = array_merge($base_args, array(
+  'category_name'  => 'ai',
+  'posts_per_page' => 2,
+));
 ?>
 
 <section class="<?php echo $ex_class . ' ' . $bg_class; ?>" id="blog">
   <article class="<?php echo $ex_inner_class; ?>">
     <?php echo usa_set_heading_linear_show('h2', $title, 'sec'); ?>
     <div class="c-anim-box--down js-scroll-show">
-      <?php usa_set_extra_sub_loop($args); ?>
+      <?php
+      usa_set_extra_sub_loop($args_sql);
+      usa_set_extra_sub_loop($args_ai);
+      ?>
     </div>
     <?php get_template_part('components/common/read-more'); ?>
   </article>
